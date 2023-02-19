0f6cd071c4da8516436f6bae76e17f1244f51a12
Omar <omarmreda95@gmail.com>
2023-02-19 21:32:58 +0200

    Submitting normal screen UI

--- src/ItemCardsContainer.js
+++ src/ItemCardsContainer.js
@@ -1,9 +1,25 @@
 import ItemCard from "./components/ItemCard";
 export default function ItemCardsContainer({ data }) {
+  const colors = [
+    "#FF8B64",
+    "#55C2E6",
+    "#FF5E7D",
+    "#FF8B64",
+    "#55C2E6",
+    "#4BCF82",
+    "#7335D2",
+    "F1C75B",
+    "#4BCF82",
+    "#7335D2",
+  ];
   return (
     <div className="card-container">
-      {data.map((item) => (
-        <div className="outer-container">
+      {data.map((item, index) => (
+        <div
+          className="outer-container"
+          key={item.id}
+          style={{ backgroundColor: colors[index] }}
+        >
           <ItemCard
             key={item.id}
             price={item.price}
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,25 +1,24 @@
 
 export default function MainCard() {
   return (
-    < >
-      <div className="main-card">
-        <div id="img"></div>
-        <img
-          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          className="profile-img"
-          alt="person"
-        />
-        <span className="profile-caption"> Report for</span>
-        <span className="mainCardHeading">Best Sales</span>
-        <div className="attached-main-card">
-          <ul id="ulAttachedMainCard">
-            <li>Daily</li>
-            <li>Weekly</li>
-            <li>Monthly</li>
-          </ul>
-        </div>
-      </div>
+    <div>
 
-    </>
+      <div className="main-card">
+        <div className="attached-main-card">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="profile-img"
+            alt="person"
+          />
+          <span className="profile-caption"> Report for</span>
+          <span className="mainCardHeading">Best Sales</span>
+        </div>
+        <ul id="ulAttachedMainCard">
+          <li>Daily</li>
+          <li>Weekly</li>
+          <li>Monthly</li>
+        </ul>
+      </div>
+    </div>
   );
 }
